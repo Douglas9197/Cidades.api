74262bbc9418cf5b711de16733bb79c1389aee3a
Douglas9197 <douglas_wilham@yahoo.com.br>
2021-12-04 21:26:36 -0300

    api cidades

---
+++ src/test/java/com/github/douglas9197/Cidadesapi/CidadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.douglas9197.Cidadesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
